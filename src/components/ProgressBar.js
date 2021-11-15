d7ca6bee187732c2e8c88f7f75a54512e4906e2d
ivanlass <ivansky56@gmail.com>
2021-11-15 13:07:18 +0100

    fixed bug on progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -9,7 +9,7 @@ function ProgressBar({width}) {
       className={width === 100 ? "inner-progress full": "inner-progress not-full"}
       style={{width:`${width}%`}}
       >
-        {width <100 && <div className="inner-progress-line" style={{left:`${width}%`}}></div>}
+        {!(width ===100 || width === 0 ) && <div className="inner-progress-line" style={{left:`${width}%`}}></div>}
     </div>
   </div>
   <p className="progress-bar-label">{width}</p>
